6f92586eebbfd89647c7b1b033cb9aed059ab235
Lucas Moreira <eskerlucas1@gmail.com>
2016-03-06 22:10:27 -0300

    criando o form de cadastro

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,12 +1,16 @@
 module.exports = function(app) {
 	app.get('/produtos', function(req, res) {
 		var connection = app.infra.connectionFactory();
-		var produtosBanco = new app.infra.ProdutosDAO(connection);
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-		produtosBanco.lista(function(err, results) {
+		produtosDAO.lista(function(err, results) {
 			res.render('produtos/lista', {lista: results});
 		});
 
 		connection.end();
 	});	
+
+	app.get('/produtos/form', function(req, res) {
+		res.render('produtos/form');
+	});
 }
