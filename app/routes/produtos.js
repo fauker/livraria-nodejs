af133102e7aefe9e8d42d37bab376b57e1ceba43
Lucas Moreira <eskerlucas1@gmail.com>
2016-03-06 22:36:25 -0300

    tirando melhor proveito do protocolo HTTP

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -13,4 +13,13 @@ module.exports = function(app) {
 	app.get('/produtos/form', function(req, res) {
 		res.render('produtos/form');
 	});
+
+	app.post('/produtos', function(req, res) {
+		var connection = app.infra.connectionFactory();
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
+		var livro = req.body;
+		produtosDAO.salva(livro, function(err, results) {
+			res.redirect('/produtos');
+		});
+	});
 }
--- config/express.js
+++ config/express.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
 
 module.exports = function() {
 	var app = express();
+
+	app.use(bodyParser.urlencoded({extended: true}));
 
 	app.set('view engine', 'ejs');
 	app.set('views', './app/views');	
