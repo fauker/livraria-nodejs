8867fb6655cc65feba0dc8c7a512eafd9fff7c40
Lucas Moreira <eskerlucas1@gmail.com>
2016-03-09 14:12:18 -0300

    Banco de test para os testes unitários

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,12 +1,23 @@
 var mysql = require('mysql');
 
 function createDBConnection() {
-	return mysql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			password: 'root',
-			database: 'casadocodigo'
-		});
+	if(!process.env.NODE_ENV){
+		return mysql.createConnection({
+				host: 'localhost',
+				user: 'root',
+				password: 'root',
+				database: 'casadocodigo'
+			});		
+	}
+
+	if(process.env.NODE_ENV == 'test') {
+		return mysql.createConnection({
+				host: 'localhost',
+				user: 'root',
+				password: 'root',
+				database: 'casadocodigo_test'
+			});		
+	}
 };
 
 module.exports = function() {
