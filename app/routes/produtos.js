d98d45770d234316fdb2c23012faae98b8fb43d8
Lucas Moreira <eskerlucas1@gmail.com>
2016-03-09 00:04:15 -0300

    Sobe

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -18,21 +18,23 @@ module.exports = function(app) {
 	});	
 
 	app.get('/produtos/form', function(req, res) {
-		res.render('produtos/form');
+		res.render('produtos/form', {errosValidacao: {}, livro: {}});
 	});
 
 	app.post('/produtos', function(req, res) {
 		req.assert('titulo', 'O título é obrigatório').notEmpty();
 
+		var livro = req.body;
+
 		var erros = req.validationErrors();
 		if (erros) {
-			res.render('produtos/form', {errosValidacao: erros});	
+			res.render('produtos/form', {errosValidacao: erros, livro:livro});	
 			return;
 		}
 
 		var connection = app.infra.connectionFactory();
 		var produtosDAO = new app.infra.ProdutosDAO(connection);
-		var livro = req.body;
+		
 		produtosDAO.salva(livro, function(err, results) {
 			res.redirect('/produtos');
 		});
